08e6d9a7842ca5feff8070cff0a11af99cab902f
ogwerset <140664646+ogwerset@users.noreply.github.com>
2023-11-14 02:59:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,45 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Replace with your MongoDB URI
+const mongoUri = 'mongodb+srv://Cluster23376:Kupasmierdzi1@secretsanta.8bfzhm1.mongodb.net/?retryWrites=true&w=majority';
 
-const cors = require('cors');
 app.use(cors());
+app.use(bodyParser.json());
 
+const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-// MongoDB Atlas connection string
-const mongoDBAtlasConnectionString = 'mongodb+srv://Cluster23376:Kupasmierdzi1@secretsanta.8bfzhm1.mongodb.net/?retryWrites=true&w=majority';
-
-mongoose.connect(mongoDBAtlasConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB Atlas...'))
-  .catch(err => console.error('Could not connect to MongoDB Atlas...', err));
-
-app.use(express.json()); // for parsing application/json
-
-const Participant = require('./participant'); // Import the Participant model
-
-// GET route to fetch participants
 app.get('/api/participants', async (req, res) => {
-  try {
-    const participants = await Participant.find();
-    res.send(participants);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+    try {
+        await client.connect();
+        const collection = client.db("secretsanta").collection("participants");
+        const participants = await collection.find({}).toArray();
+        res.json(participants);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
 });
 
-// POST route to add a new participant
 app.post('/api/participants', async (req, res) => {
-  const participant = new Participant({
-    name: req.body.name,
-    preferences: req.body.preferences,
-    slot: req.body.slot
-  });
-
-  try {
-    const savedParticipant = await participant.save();
-    res.send(savedParticipant);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+    try {
+        await client.connect();
+        const collection = client.db("secretsanta").collection("participants");
+        const result = await collection.insertOne(req.body);
+        res.status(201).json(result.ops[0]);
+    } catch (error) {
+        res.status(500).send(error.message);
+    } finally {
+        await client.close();
+    }
 });
-``
-
-// Define your routes and middleware here
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
